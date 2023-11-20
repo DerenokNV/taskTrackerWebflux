08430eac26afbb2dde293ae652ee165445fe5d7a
DerenokNV <DerenokNV@mail.ru>
2023-11-20 18:52:54 +0500

    костяк

---
+++ src/main/java/com/example/tasktrackerwebflux/TaskTrackerWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasktrackerwebflux/TaskTrackerWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
